31184350a5db52f4f7736dda692134f9122f4b89
faraz12948 <faraz15-12948@diu.edu.bd>
2021-12-17 16:00:55 +0600

    bug fixed

--- src/pages/HomePage/Review/Review.js
+++ src/pages/HomePage/Review/Review.js
@@ -3,23 +3,10 @@ import { Row } from 'react-bootstrap';
 import ShowReviews from '../ShowReviews/ShowReviews';
 
 const Review = () => {
-    const [reviews,setReviews]=useState([])
-    useEffect(()=>{
-        fetch('https://stormy-gorge-80832.herokuapp.com/home/review')
-        .then(res=>res.json())
-        .then(data=>setReviews(data))
 
-    },[])
     return (
         <div className="container mt-5">
-            <h3 className="my-5"><b>Top</b><span style={{color:'goldenrod' , fontWeight:'bold'}}> Reivews</span></h3>
-           <div >
-           <Row xs={1} md={2} className="g-4">
-           {
-                reviews?.map(review => <ShowReviews key={review._id} review={review}></ShowReviews>)
-           }
-           </Row>
-           </div>
+
             
         </div>
     );
